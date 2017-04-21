aaa2538a1c97db02fa9237b6c40c0578449b6467
Prashantagrwal <agrawalprashant1995@gmail.com>
2017-04-22 03:05:29 +0530

    Update ServerFile.java

--- navigate/ServerFile.java
+++ navigate/ServerFile.java
@@ -4,12 +4,12 @@ package com.example.dell.navigate;
 public class ServerFile
 {
 
-    String user_email_id="https://marutilive.000webhostapp.com/user_email.php?";
-    String user_cart_fetch="https://marutilive.000webhostapp.com/fetch_cart.php?";
-    String user_cart_add="https://marutilive.000webhostapp.com/add_cart.php?";
-    String fetch_product="https://marutilive.000webhostapp.com/fetch_product.php?";
-    String enquiry_orders="https://marutilive.000webhostapp.com/enquiry_orders.php?";
-    String user_main="https://marutilive.000webhostapp.com/main_view.php";
-    String image_php="https://marutilive.000webhostapp.com/images/";
-    String order_status="https://marutilive.000webhostapp.com/order_status.php";
+    String user_email_id="";
+    String user_cart_fetch="";
+    String user_cart_add="";
+    String fetch_product="";
+    String enquiry_orders="";
+    String user_main="";
+    String image_php="";
+    String order_status="";
 }
